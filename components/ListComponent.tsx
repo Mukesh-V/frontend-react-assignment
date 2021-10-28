b04f645664dad05e65b8dc3ac22a716feac91549
Mukesh V <mukeshvinayak2001@gmail.com>
2021-10-28 15:32:10 +0530

    added material ui

--- components/ListComponent.tsx
+++ components/ListComponent.tsx
@@ -1,28 +1,71 @@
+import { 
+  Paper,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Table } from "@mui/material";
+import { withStyles } from "@mui/styles";
 import React, { useContext } from "react";
 import CoordinateContext from './CoordinateContext';
 
 type Props = {
   className?: string;
+  classes: {};
 };
 
 const ListComponent = (props: Props): JSX.Element => {
   var { markers, setMarkers } = useContext(CoordinateContext)
   return (
     <div className={props.className}>
-      <h3>Bookmarked Place</h3>
-      <ul>
-        {
-          markers.coords.map((item, key)=>{
-            return (
-              <li key={key} onClick={()=>{(markers.selected!=key)?setMarkers({...markers, selected: key}) : setMarkers({...markers, selected: -1})}}>
-                {key}, {item.lng}, {item.lat}
-              </li>
-            );
-          })
-        }
-      </ul>
+      <h3>Bookmarked Places</h3>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 350, maxHeight:'80vh'}} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Index</TableCell>
+            <TableCell align="center">Longitude</TableCell>
+            <TableCell align="center">Latitude</TableCell>
+            <TableCell align="center">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+              markers.coords.map((item, key) => (
+                <TableRow
+                  key={key}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  onClick={()=>{markers.selected!=key?setMarkers({...markers, selected: key}):setMarkers({...markers, selected:-1})}}
+                  style={{backgroundColor:markers.selected!=key?'white':'black'}}
+                >            
+                  <TableCell className={markers.selected!=key?props.classes.cell : props.classes.selectedCell}>{key}</TableCell>
+                  <TableCell className={markers.selected!=key?props.classes.cell : props.classes.selectedCell}>{item.lng}</TableCell>
+                  <TableCell className={markers.selected!=key?props.classes.cell : props.classes.selectedCell}>{item.lat}</TableCell>
+                  <TableCell className={markers.selected!=key?props.classes.cell : props.classes.selectedCell}>
+                    
+                  </TableCell>
+                </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   );
 };
 
-export default ListComponent;
+
+const ListStyles = {
+  cell:{
+    textAlign: 'center',
+    cursor: 'pointer'
+  },
+  selectedCell:{
+    color: 'white',
+    cursor: 'pointer',
+    textAlign: 'center'
+  }
+}
+
+export default withStyles(ListStyles)(ListComponent);
--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -2,6 +2,8 @@ import React, { useState, useContext } from "react";
 import ReactMapGL, { MapContext, MapEvent, ViewportProps } from "react-map-gl";
 import CoordinateContext, { Coordinate } from './CoordinateContext';
 
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
 interface MapComponentProps {
   className?: string;
 }
@@ -20,26 +22,40 @@ function CustomMarker(props: MarkerProps) {
   var {point, index, selected} = props;
   const [x, y] = context.viewport? context.viewport.project([point.lng, point.lat]) : [];
 
-  var unselectedMarkerStyle : React.CSSProperties = {
+  var unselectedIndexStyle : React.CSSProperties = {
+    fontSize: '0.7em',
     position: 'absolute',
-    background: 'white',
-    left: x,
-    top: y
+    background: 'transparent',
+    left: x-4.5,
+    top: y-3
   }
 
+  var unselectedMarkerStyle : React.CSSProperties = {
+    fontSize: '1.4em',
+    position: 'absolute',
+    background: 'transparent',
+    left: x-15,
+    top: y-28
+  }
+
+  // for a deepcopy of an object
+  var selectedIndexStyle : React.CSSProperties = JSON.parse(JSON.stringify(unselectedIndexStyle));
   var selectedMarkerStyle : React.CSSProperties = JSON.parse(JSON.stringify(unselectedMarkerStyle));
-  selectedMarkerStyle.background = 'red';
+  selectedIndexStyle.color = 'red'; selectedMarkerStyle.color = 'red';
 
   return (
-    <div style={selected===index? selectedMarkerStyle : unselectedMarkerStyle} >
-      {index}
+    <div>
+      <LocationOnIcon style={selected===index? selectedMarkerStyle : unselectedMarkerStyle}/>
+      <div style={selected===index? selectedIndexStyle : unselectedIndexStyle}>
+        {index}
+      </div>
     </div>
   );
 }
 
 const MapComponent = (props: MapComponentProps): JSX.Element => {
   const [viewport, setViewport] = useState<ViewportProps>({
-    width: 450,
+    width: 800,
     height: 450,
     latitude: 37.7577,
     longitude: -122.4376,
@@ -61,10 +77,12 @@ const MapComponent = (props: MapComponentProps): JSX.Element => {
           var clickpoint : Coordinate = { lng: e.lngLat[0], lat: e.lngLat[1]};
 
           // for some reason, .includes() didn't work as expected
-          if(!markers.coords.find( point => (point.lat === clickpoint.lat &&  point.lng === clickpoint.lng))){
+          if(!markers.coords.find( point => (point.lat === clickpoint.lat &&  point.lng === clickpoint.lng)))
             setMarkers({...markers, coords:[...markers.coords, clickpoint]});
-          }
+          else
+            setMarkers({...markers, selected:-1});
         }}
+        style={{margin: 'auto'}}
       >
       {
         markers.coords.map((value, key)=>{
